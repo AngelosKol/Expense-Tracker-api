1a5add9f7e6187dc65627c1c2c92700ab8a18643
unknown <koliosor@gmail.com>
2025-05-03 15:44:04 +0300

    fix: - Minor fixes

--- src/main/java/com/ang/rest/domain/entity/Category.java
+++ src/main/java/com/ang/rest/domain/entity/Category.java
@@ -28,6 +28,12 @@ public class Category {
 
     @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<Product> products ;
+
+    public Category(Long id, String name, ProductFamily family) {
+        this.id = id;
+        this.name = name;
+        this.family = family;
+    }
     public boolean equals(final Object o) {
         if (o == this) return true;
         if(o == null || getClass() != o.getClass()) return false;
