84fd830594d823f14436bccd35afbd4a73a9fcac
Angelos <koliosor@gmail.com>
2025-06-05 09:51:52 +0300

    refactor: - Removes comments and unused imports

--- src/main/java/com/ang/rest/product/ProductServiceImpl.java
+++ src/main/java/com/ang/rest/product/ProductServiceImpl.java
@@ -64,7 +64,6 @@ public class ProductServiceImpl implements ProductService {
     @Override
     public Page<ProductDTO> findAll(String filter, Pageable pageable) {
         return productRepository.findByNameContainingIgnoreCase(filter,pageable, ProductDTO.class);
-//        return productRepository.findByNameContainingIgnoreCase(filter,pageable).map(productMapper::mapToDto);
     }
     @Override
     public List<Product> findAllByID(List<Long> productIDList) {
